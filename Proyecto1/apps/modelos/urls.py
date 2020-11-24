798613b72494758c9ff4ac3651b3292fc63dc23b
Daniel Zarate <danifado@gmail.com>
2020-11-24 17:38:41 -0500

    Se borra version defectuosa de la aplicacion

--- Proyecto1/apps/modelos/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apps.modelos.views import index
-
-urlpatterns = {
-    path("", index)
-}
--- pizzaTime/apps/Domicilios/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DomiciliosConfig(AppConfig):
-    name = 'Domicilios'
